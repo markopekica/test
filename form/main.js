c8b7b6f14194d7e58f1f0ba79f72ec48496bd143
marko <pekica.marko@gmail.com>
2020-10-29 22:30:08 +0100

    Added meta tags Attempt to get better font size Cleaned unecessary

---
+++ form/main.js
@@ -0,0 +1,16 @@
+// var quotes = [
+//     'The most disgraceful kind of loss, however, is that due to carelessness.', //letter 1
+//     'for such restlessness is the sign of a disordered spirit.',    //letter 2
+//     'Everywhere means nowhere.', //letter 2
+//     'Accordingly, since you cannot read all the books which you may possess, it is enough to possess only as many books as you can read.', //letter 2
+//     'It is equally faulty to trust everyone and to trust no one.', //letter 3
+//     'I do not yet, however, assure myself, or indulge the hope, that there are no elements left in me which need to be changed.',  //letter 6
+//     'I can show you many who have lacked, not a friend, but a friendship;', //letter 6
+//
+// ]
+// var randomNumber = Math.floor(Math.random() *42 /69);
+//
+// function NewQuote() {
+//     var randomNumber = Math.floor(Math.random() * (quotes.length));
+//     document.getElementById('qte').innerHTML = quotes[randomNumber];
+// }
